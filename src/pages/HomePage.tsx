7d7116f49d10c3c30155e3c2ce65f59c0a85a247
Fatema Afroza <fafroza2017@gmail.com>
2025-09-04 21:27:32 +0200

    Placeholder till 'registrera' & 'login' inne i homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,15 @@
 
 export default function HomePage() {
   return (
+    // Div med bakgrundsgradient och centrerad innehåll
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-500 to-purple-600">
+        {/* Rutan där visas texter, 'registrera' och 'login'  */}
         <div className="bg-white p-10 rounded-2xl shadow-lg text-center">
+            {/* Rubriken, kan lägga till roliga ikoner sen! */}
             <h1 className="text-4xl font-bold text-gray-800">
                 Welcome to PictureThis!
             </h1>
-            {/* Välkommstext till PictureThis! */}
+            {/* Välkommstexter till PictureThis! */}
             <p className="mt-4 text-gray-600">
                 Ett roligt ritspel där en spelare ritar och de andra gissar vad det är i chatten.
                 <br />
@@ -14,6 +17,11 @@ export default function HomePage() {
                 <br />
                 Ha det så kul!
             </p>
+            
+            {/* Här inne i div:en ska lägga till 'registrera' & 'login' */}
+            <div>
+
+            </div>
 
         </div>
 
