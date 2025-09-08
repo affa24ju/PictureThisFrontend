70c8de53fde7d6330c5ec81e8cb1239ab45443b9
Monika-feg <mala88@hotmail.comcd>
2025-09-08 10:43:51 +0200

    nu bör chat funktionen fungera, vi får testa sen när vi kan logga in

--- src/utils/UseChatClient.ts
+++ src/utils/UseChatClient.ts
@@ -1,14 +1,44 @@
 import { useEffect, useState } from "react";
 import { StompClient } from "./StompConnect";
 
+interface ChatMessage {
+    content : string;
+    user: {
+        id: string;
+        userName: string;
+    }
+}
 
 export function useChatClient() {
-    const[message, setMessage] = useState<string | null>(null);
+    const [messages, setMessages] = useState<ChatMessage[]>([]);
 
     useEffect(()=>{
         StompClient.onConnect = () =>{
             console.log("Connected to stomp");
+
+            StompClient.subscribe('/topic/messages', (greetings)=>{
+                console.log("New message received" + greetings.body);
+                const pars = JSON.parse(greetings.body);
+                console.log("received message: ", pars);
+                setMessages((p) => [...p, pars]);
+            });
+            console.log()
         }
         StompClient.activate();
     }, [])
+
+    const sendMessage = (greetings:string)=>{
+        StompClient.publish({
+            destination: '/app/chat',
+            body: JSON.stringify({
+                content: greetings,
+                user: {
+                    id: "user_id",
+                    userName: "user_name"
+                }
+            })
+        });
+    }
+
+    return {messages, sendMessage}
 }
