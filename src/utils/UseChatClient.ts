48f11e6d2e630d0787ed23fdff156686e320c77e
adNord <adno96@gmail.com>
2025-09-10 08:52:15 +0200

    tog bort kommentar

--- src/utils/UseChatClient.ts
+++ src/utils/UseChatClient.ts
@@ -44,41 +44,6 @@ export function useChatClient() {
     };
   }, []);
 
-/*
-    useEffect(()=>{
-        StompClient.onConnect = () =>{
-            console.log("Connected to stomp");
-
-            StompClient.subscribe('/topic/messages', (greetings)=>{
-                console.log("Sub callback fired!", greetings);
-                console.log("New message received" + greetings.body);
-                const pars = JSON.parse(greetings.body);
-                console.log("received message: ", pars);
-                setMessages((p) => [...p, pars]);
-            });
-            console.log()
-        }
-        StompClient.activate();
-
-        return () =>{
-            StompClient.deactivate();
-        }
-    }, [])
-
-    const sendMessage = (greetings:string)=>{
-        StompClient.publish({
-            destination: '/app/chat',
-              body: JSON.stringify({
-              userName: currentUser.userName,
-              messageContent: greetings
-            })
-        });
-    }
-
-    return {messages, sendMessage}
-}
-*/
-
     const sendMessage = (greetings: string) => {
          if (!connected) {
     console.warn("Chat not connected yet");
